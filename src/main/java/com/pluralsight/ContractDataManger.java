0f466efbccaf6daf3d761572a8f87b3422206a67
Brandonmorsework <brandonmorsework@gmail.com>
2025-05-19 08:18:59 -0400

    working saveContract method

--- src/main/java/com/pluralsight/ContractDataManger.java
+++ src/main/java/com/pluralsight/ContractDataManger.java
@@ -1,7 +1,25 @@
 package com.pluralsight;
 
-public class ContractDataManger {
+import java.util.ArrayList;
 
+public class ContractDataManager {
 
+    public static void main(String[] args) {
+
+        ArrayList<Contract> contracts = new ArrayList<>();
+
+        contracts.add(new SalesContract());
+        contracts.add(new LeaseContract());
+
+        for (int i = 0; i < 2; i++) {
+
+            if (contracts.get(i) instanceof SalesContract) {
+                contracts.add()
+            }
+            if (contracts.get(i) instanceof LeaseContract) {
+                contracts.add();
+            }
+        }
+    }
 
 }
--- src/main/java/com/pluralsight/ContractFileManager.java
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -5,20 +5,38 @@ import java.io.*;
 
 public class ContractFileManager {
 
-    public void saveDealership(Dealership dealership) {
+    public void saveContract(Contract contract) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("contracts.csv"))) {
 
-            bw.write(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhone());
-            bw.newLine();
+        String date = contract.getDate();
+        String name = contract.getName();
+        String email = contract.getEmail();
+        boolean isSold = contract.isVehicleSold();
+        Vehicle vehicle = contract.getVehicle();
 
-            for (Vehicle vehicle : dealership.getAllVehicles()) {
-                bw.write(vehicle.getVin() + "|" + vehicle.getYear() + "|" + vehicle.getMake() + "|" + vehicle.getModel()
-                        + "|" + vehicle.getVehicleType() + "|" + vehicle.getColor() + "|" + vehicle.getOdometer()
-                        + "|" + vehicle.getPrice());
-                bw.newLine();
+        String vehicleInfo = String.format("%s|%d|%s|%s|%s|%s|%d|%.2f",
+            vehicle.getVin(),
+            vehicle.getYear(),
+            vehicle.getMake(),
+            vehicle.getModel(),
+            vehicle.getVehicleType(),
+            vehicle.getColor(),
+            vehicle.getOdometer(),
+            vehicle.getPrice());
+
+
+
+            if (contract instanceof SalesContract) {
+
+
+
+
+        } else if (contract instanceof LeaseContract){
+
             }
 
             System.out.println("Contract saved successfully to contracts.csv.");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
